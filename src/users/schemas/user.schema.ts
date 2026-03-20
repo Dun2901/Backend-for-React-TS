08eed6be2ba62c92139715e9eefa4464cb7ae6e9
Dun2901 <lamchidung373@gmail.com>
2026-03-20 22:51:12 +0700

    generate CRUD and Create Schema User

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,39 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop()
+  fullName: string;
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  phone: string;
+
+  @Prop()
+  role: string;
+
+  @Prop()
+  avatar: string;
+
+  @Prop()
+  isActive: boolean;
+
+  @Prop()
+  type: string;
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  updateAt: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
