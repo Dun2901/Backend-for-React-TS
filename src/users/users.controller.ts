a7efc7892dcc91330c337698d6e41742d1bfaf4f
Dun2901 <lamchidung373@gmail.com>
2026-03-20 23:26:55 +0700

    test create new user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,8 +16,8 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  create(@Body('email') myEmail: string) {
+    return this.usersService.create(myEmail);
   }
 
   @Get()
