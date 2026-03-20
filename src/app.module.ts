c7a298eeac3aeccd90eb93efd47d738bb30ca465
Dun2901 <lamchidung373@gmail.com>
2026-03-20 21:08:43 +0700

    connect to mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://Dung2901:PdQDwrI3KXYgJ0LL@cluster0.xlpwb.mongodb.net/',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
